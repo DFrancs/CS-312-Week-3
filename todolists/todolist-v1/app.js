4cbd18f393e8131a835d31638bb178595398260b
DFrancs <dkf55@nau.edu>
2024-06-23 17:49:55 -0700

    Final update for todolist

--- todolists/todolist-v1/app.js
+++ todolists/todolist-v1/app.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.get('/', function (req, res) {
-    let today = new Date();
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-    let day = today.toLocaleDateString('en-US', options);
-
+    const day = date.getDate();
     res.render('list', { listTitle: day, newListItems: items });
 });
 
@@ -26,7 +20,7 @@ app.get('/work', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === "Work List") {
         workItems.push(item);
         res.redirect('/work');
@@ -39,7 +33,6 @@ app.post('/', function (req, res) {
 app.get('/about', function (req, res) {
     res.render('about');
 });
-
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000.");
